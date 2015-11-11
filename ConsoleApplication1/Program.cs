2bb62e90a908012667e3654960b065ce76b6b143
TestCat <serverov2014@yandex.ru>
2015-11-11 16:19:15 +0400

    5

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = Utils.getSquareRect(44, 59);
+            Console.WriteLine("New vers square:" + square);
+
+            Console.WriteLine("press any key...");
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ ConsoleApplication1/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Utils
+    {
+        public static int getSquareRect(int a, int b)
+        {
+            return (a * b) / 2;
+        }
+
+    }
+}
